462d867135f9edcfaa2db01522bcfdd064f8d8c6
Fullenbaum_cp <SND\Fullenbaum_cp>
2013-08-29 22:07:33 +0000

    First upload. V1.

---
+++ FilesCount/FilesCount/ArgsChecker.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace FilesCount
+{
+    // Error list
+    public enum ArgsErrors { NoError = 0, InvalidArgsCount = 1, UriBadFormat = 2, DirNotFound = 3, badOption = 4 };
+
+    class ArgsChecker
+    {
+        string DirPath;
+        string ReportsPath;
+        bool AreReportsRequired;
+        bool IsSplitted;
+        bool IsStamp;
+        // Command line arguments' check
+        public ArgsErrors CheckAndInit(string[] args, ref string dirPath, ref string reportsPath, ref bool areReportsRequired, ref bool isSplitted, ref bool isStamp)
+        {
+            ArgsErrors rc = ArgsErrors.NoError;
+
+            switch (args.Length)
+            {
+                case 0:
+                    rc = ArgsErrors.InvalidArgsCount;
+                    break;
+                case 1:
+                    DirPath=dirPath = args[0];
+                    AreReportsRequired = areReportsRequired = false;
+                    if (!Directory.Exists(dirPath)) rc = ArgsErrors.DirNotFound;
+                    break;
+                case 2:
+                    DirPath = dirPath = args[0];
+                    ReportsPath = reportsPath = args[1];
+                    AreReportsRequired = areReportsRequired = true;
+                    IsSplitted = isSplitted = false;
+                    IsStamp = isStamp = false;
+                    if ((!Directory.Exists(reportsPath)) || (!Directory.Exists(dirPath))) rc = ArgsErrors.DirNotFound;
+                    break;
+                case 3:
+                    DirPath = dirPath = args[0];
+                    ReportsPath = reportsPath = args[1];
+                    AreReportsRequired = areReportsRequired = true;
+                    IsSplitted = isSplitted = false;
+                    IsStamp = isStamp = false;
+                    switch (args[2].ToUpper())
+                    {
+                        case "SPLIT":
+                            IsSplitted = isSplitted = true;
+                            break;
+                        case "STAMP":
+                            IsStamp = isStamp = true;
+                            break;
+                        default:
+                            rc = ArgsErrors.badOption;
+                            break;
+                    }
+                    if ((!Directory.Exists(reportsPath)) || (!Directory.Exists(dirPath))) rc = ArgsErrors.DirNotFound;
+                    break;
+                default:
+                    rc = ArgsErrors.InvalidArgsCount;
+                    break;
+            }
+            return rc;
+        }
+
+        // Display params
+        public void DisplayParams()
+        {
+            Output displayer = new Output();
+            displayer.WriteLine("Path to count: " + DirPath);
+            if (AreReportsRequired)
+            {
+                displayer.WriteLine("Reports path: " + ReportsPath);
+                if (IsSplitted)
+                    displayer.WriteLine("Generated file: multiple #.EXT.COUNTER");
+                else
+                {
+                    if (IsStamp)
+                        displayer.WriteLine("Generated files: one .csv with timestamp in the name");
+                    else
+                        displayer.WriteLine("Generated files: one .csv");
+                }
+            }
+        }
+
+    }
+}
---
+++ FilesCount/FilesCount/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+
+namespace FilesCount
+{
+    class Program
+    {
+        const int ENUMERATEFILES_ERROR = 99;
+
+        static void Main(string[] args)
+        {
+            string dirPath="";
+            string reportsPath="";
+            bool areReportsRequired = false;
+            bool isSplitted=false;
+            bool isStamp=false;
+
+            // Check params
+            ArgsChecker checker = new ArgsChecker();
+            ArgsErrors rc = checker.CheckAndInit(args, ref dirPath, ref reportsPath, ref areReportsRequired, ref isSplitted, ref isStamp);
+
+            // I/O Manager
+            Output output = new Output();
+
+            if (rc==ArgsErrors.NoError)
+            {
+                // Echo parameters
+                checker.DisplayParams();
+
+                // Count results
+                IDictionary<string, int> results = new Dictionary<string, int>();
+
+                int dirCount = 0;
+                long fileCount=0;
+
+                Stopwatch chrono = new Stopwatch();
+                chrono.Start();
+
+                foreach (var packdir in Directory.EnumerateDirectories(dirPath))
+                {
+                    dirCount++;
+                    Console.WriteLine(dirCount+" "+Path.GetFileName(packdir));
+                    try
+                    {
+                        foreach (var file in Directory.EnumerateFiles(packdir, "*.*", SearchOption.AllDirectories))
+                        {
+                            fileCount++;
+                            string ext = Path.GetExtension(file).ToUpper();
+                            if (results.ContainsKey(ext)) results[ext]++;
+                            else results.Add(ext, 1);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        output.DisplayError(ENUMERATEFILES_ERROR, " "+e.Message);
+                    }
+                }
+
+                chrono.Stop(); TimeSpan ts = chrono.Elapsed;
+
+                // Store counters to files
+                if (!areReportsRequired)
+                {
+                    // Display counters to the screen
+                    output.DisplayCounters(results, fileCount);
+                }
+                else
+                {
+                    string prefix = Path.GetFileName(dirPath);
+                    output.SaveCounters(results, prefix, reportsPath, isSplitted, isStamp, fileCount);
+                    output.DisplayResultFilePath(prefix, reportsPath, isSplitted, isStamp);
+                }
+                // Display time used to count
+                output.WriteLine(String.Format("Counted in : {0:00} h {1:00} m {2:00} s {3:00} ms", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10));
+            }
+            else
+            {
+                // No valid params, display doc
+                output.DisplayError(rc);
+                output.DisplayDoc();
+            }
+
+        }
+
+    }
+}
