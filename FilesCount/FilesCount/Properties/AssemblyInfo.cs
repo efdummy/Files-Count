28f49cb97c4bcbb5156ae9983f4f24a8e80f9136
Fullenbaum_cp <SND\Fullenbaum_cp>
2013-08-29 22:12:11 +0000

    AssemblyInfo update with Copyright name

--- FilesCount/FilesCount/Properties/AssemblyInfo.cs
+++ FilesCount/FilesCount/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FilesCount")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Files count")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Fullenbaum")]
 [assembly: AssemblyProduct("FilesCount")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © 2013 Fullenbaum")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
